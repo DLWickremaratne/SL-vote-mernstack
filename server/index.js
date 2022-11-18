2ea0f33ff9089428c324b3cf3ff2323daf2f454c
DLWickremeratne <dhanithlak@gmail.com>
2022-11-18 16:00:10 +0530

    First commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express'); //import express
+const handle = require('./handlers');
 
 const app = express(); // call app
-const port = 6969;//any number above 1000
+const port = 3000;//any number above 1000
 
-app.listen();
-hhhhhhhhh
+
+app.get('/', (req, res) => res.send('hello world'));
+
+
+app.use(handle.notFound);
+
+ 
+app.use(handle.error);
+
+app.listen(port, console.log(`Server started on port ${port}`));
